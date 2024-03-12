d1fe1f2a1b6e187b0f6a295c127e4f9c38233095
jhealey <jjheals@gmail.com>
2024-03-12 14:49:21 +0100

    Styles

--- industrialgiudecca/src/App.js
+++ industrialgiudecca/src/App.js
@@ -30,6 +30,10 @@ function App() {
                     <meta charSet="utf-8" />
                     <title>Industrial Giudecca</title>
                     
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                    <link href="https://fonts.googleapis.com/css2?family=Judson:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
+                    
                     <link rel="stylesheet" href="https://js.arcgis.com/4.28/esri/themes/light/main.css" />
                     <script src="https://js.arcgis.com/4.28/"></script>
                     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
