869e6cff8d15b26251f45d3f1ec2ffe52bdc58f9
jhealey <jjheals@gmail.com>
2024-03-12 14:59:06 +0100

    Imports

--- industrialgiudecca/src/App.js
+++ industrialgiudecca/src/App.js
@@ -36,6 +36,9 @@ function App() {
                     <meta charSet="utf-8"/>
 
                     <title>Industrial Giudecca</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                    <link href="https://fonts.googleapis.com/css2?family=Judson:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
                     
                     <link rel="stylesheet" href="https://js.arcgis.com/4.28/esri/themes/light/main.css" />
                     <script src="https://js.arcgis.com/4.28/"></script>
