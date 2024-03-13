fd167ce1ff7394f47a499f903fbe8e0d439d554f
jhealey <jjheals@gmail.com>
2024-03-13 10:38:26 +0100

    Sidebar styling (3)

--- industrialgiudecca/src/App.js
+++ industrialgiudecca/src/App.js
@@ -12,29 +12,8 @@ function App() {
 
     return (
         <div className="App">
-            <header>
-                <button className="hamburger" onClick={toggleSidebar}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Judson"/>
-
-                <link rel="stylesheet" href="https://js.arcgis.com/4.28/esri/themes/light/main.css"/>
-                <script src="https://js.arcgis.com/4.28/"></script>
-                <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-                <script type="module" src="https://js.arcgis.com/calcite-components/1.9.2/calcite.esm.js"></script>
-                <link rel="stylesheet" type="text/css" href="https://js.arcgis.com/calcite-components/1.9.2/calcite.css"/>
-                <link rel="stylesheet" href="../static/index.css"/>
-            </header>
-
             <head>
-                <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no"/>
-                <meta charSet="utf-8"/>
-                <title>Industrial Giudecca</title>
+                
             </head>
 
             <div><Sidebar isOpen={showSidebar}/></div>
