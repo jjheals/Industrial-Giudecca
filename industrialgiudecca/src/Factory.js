3a5a46249921aa5fb65fb1a9e2474d28f665064a
jhealey <jjheals@gmail.com>
2024-03-15 09:08:36 +0100

    begin adding arcgis api calls

---
+++ industrialgiudecca/src/Factory.js
@@ -0,0 +1,39 @@
+class Factory { 
+
+    /** Factory(attributes, geometry)
+     * @abstract 
+     * @param {dict} attributes 
+     * @param {dict} geometry 
+     */
+    constructor(attributes, geometry) { 
+        console.log("New factory");
+        this.OBJECTID = attributes.OBJECTID;
+        this.Opening_Date = attributes.Opening_Date;
+        this.Closing_Date = attributes.Closing_Date;
+        this.English_Name = attributes.English_Name;
+        this.Italian_Name = attributes.Italian_Name;
+        this.Factory_Description = attributes.Factory_Description;
+        this.Max_Employmenet = attributes.Max_Employment;
+        this.Factory_ID = attributes.Factory_ID;
+        this.Factory_Active = attributes.Factory_Active;
+        this.Building_ID = attributes.Building_ID;
+        this.x_coord = geometry.x;
+        this.y_coord = geometry.y;
+    }
+
+    toString() { 
+        let s = `Factory: ${this.English_Name} (${this.Factory_ID})\n`;
+        s += `\tOpening Date: ${this.Opening_Date}\n`;
+        s += `\tClosing Date: ${this.Closing_Date}\n`;
+        s += `\tEnglish Name: ${this.English_Name}\n`;
+        s += `\tItalian Name: ${this.Italian_Name}\n`;
+        s += `\tFactory Description: ${this.Factory_Description}\n`;
+        s += `\tMax Employment: ${this.Max_Employment}\n`;
+        s += `\tFactory ID: ${this.Factory_ID}\n`;
+        s += `\tFactory Active: ${this.Factory_Active}\n`;
+        s += `\tBuilding ID: ${this.Building_ID}\n`;
+        s += `X Coord: ${this.x_coord}\n`;
+        s += `Y Coord: ${this.y_coord}\n`;
+        return s;
+    }
+}
--- industrialgiudecca/src/pages/FactoryHomepage.js
+++ industrialgiudecca/src/pages/FactoryHomepage.js
@@ -3,88 +3,41 @@ import React from 'react';
 import '../css/FactoryHomepage.css';
 import { Link } from 'react-router-dom';
 
-function FactoryHomepage() {
+/** fetchFactoriesFL(serviceURL) 
+ * @abstract Fetch the "FactoriesFL" using the ArcGIS service endpoint given
+ * @param {string} serviceURL 
+ * @returns {dict} dictionary of the { features, attributes }
+ */
+async function fetchFactoriesFL(serviceURL, apiToken) { 
+    const authentication = arcgisRest.ApiKeyManager.fromKey(apiToken);
+    let factories = []
 
-    /** fetchFactoriesFL(serviceURL) 
-     * @abstract Fetch the "FactoriesFL" using the ArcGIS service endpoint given
-     * @param {string} serviceURL 
-     * @returns {dict} dictionary of the { features, attributes }
-     */
-    async function fetchFactoriesFL(serviceURL) { 
-        try {
-            // Hit arcGIS service endpoint to get the feature layer data
-            const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // CORS proxy service URL
-            const response = await fetch(proxyUrl + featureLayerUrl);
-            if (!response.ok) {
-              throw new Error('Failed to fetch data');
-        } 
+    // Hit ArcGIS service endpoint 
+    arcgisRest.queryFeatures({
+        url: serviceURL,
+        authentication
+    })
 
-        console.log(response);
+    // Handle response
+    .then((response) => {
+        console.log(response.features); // DEBUG
 
-        // Extract the json from the response 
-        const layerJson = await response.json();
+        // Convert the response's features into Factories
+        response.features.forEach(feature => {
+            console.log("Feature");
+            console.log(feature);
 
-        // Validate data format 
-        if (!layerJson.hasOwnProperty('fields') || !layerJson.hasOwnProperty('features')) { 
-            throw new Error('Invalid FL format.');
-        }
-
-        // Extract the attribute fields
-        const fields = layerJson.fields.map(field => field.name);
-
-        // Extract the attribute data 
-        const attributes = layerJson.features.map(feature => { 
-            const attribute = {};
-            fields.forEach(field => { 
-                attribute[field] = layerJson.features.attributes[field];
-            }); 
-        })
-
-        // Format and return fields and attributes
-        return { fields, attributes };
-
-        // Catch errors
-        } catch(error) { 
-            console.log("Error fetching FL data.", error);
-            return null;
-        }
-    }
-
-    
-
-    const featureLayerUrl = 'https://services7.arcgis.com/EXxkqxLvye8SbupH/arcgis/rest/services/Factories_FL_2/FeatureServer';
-    console.log("Fetching FL data")
-
-    fetchFactoriesFL(featureLayerUrl)
-    .then(data => {
-        if (data) {
-        console.log('Attribute fields:', data.fields);
-        console.log('Attribute data:', data.attributes);
-        
-        // Convert to CSV format or perform other operations
-        // Example: Convert to CSV
-        const csv = data.fields.join(',') + '\n' +
-                    data.attributes.map(attribute => data.fields.map(field => attribute[field]).join(',')).join('\n');
-        console.log('CSV:', csv);
-        }
+            const factory = new Factory(feature.attributes, feature.geometry)
+            factories.concat(factory);
+            console.log(factory.toString());
+        });
+        document.getElementById("result").textContent = JSON.stringify(response.features, null, 2);
     });
 
+    return factories;
+}
 
-
-
-
-
-    const landscapeData = [
-        { id: 1, name: 'Stucky Factory', link: '/stucky-factory' },
-        { id: 2, name: 'Factory 2', link: '/factory2' },
-        { id: 3, name: 'Factory 3', link: '/factory3' },
-        { id: 4, name: 'Factory 4', link: '/factory4' },
-        { id: 5, name: 'Factory 5', link: '/factory5' },
-        { id: 6, name: 'Factory 6', link: '/factory6' },
-        { id: 7, name: 'Factory 7', link: '/factory7' },
-        { id: 8, name: 'Factory 8', link: '/factory8' },
-        { id: 9, name: 'Factory 9', link: '/factory9' },
-    ];
+function FactoryHomepage() {
 
     return (
         <div className="factory-homepage">
@@ -96,6 +49,10 @@ function FactoryHomepage() {
                     </ul>
                 </nav>
             </header>
+
+            <head>
+                <script src="Factory.js"></script>
+            </head>
 
             <main>
                 <section className="landscape-grid">
