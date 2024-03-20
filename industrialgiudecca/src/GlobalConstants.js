36cfd12e43ce956a9fa1d0ad6789d711b093a8a4
jhealey <jjheals@gmail.com>
2024-03-20 12:00:02 +0100

    key

--- industrialgiudecca/src/GlobalConstants.js
+++ industrialgiudecca/src/GlobalConstants.js
@@ -1,6 +1,6 @@
 
 // API key with access to the below serviceURL
-const apiKey = '3NKHt6i2urmWtqOuugvr9eRWfrMvD-5Ec3blhM1-ZTiOsFpF5CJML56J1EnLLHaWhttLen5zn_Z7tNXT3KUkWqzy01cTGf9m2hoNPKMPDaqt3oPAEoeEev5hqoJGwNL7';
+const apiKey = '3NKHt6i2urmWtqOuugvr9YxLPcy67CXR0gve-12K7zjq2aFB-EvSxO305knAkbJ6poG6ij9UfS9KtX7q4oKNpLHZLl0Hglf-cliBrk8XeGzSJr28PUUiWqv93vrDt4x1';
 
 // URL endpoint to the service hosting the FactoriesFL
 const factoriesServiceURL = 'https://services7.arcgis.com/EXxkqxLvye8SbupH/arcgis/rest/services/Factories_FL_2/FeatureServer/0';
