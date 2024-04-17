6185389faa2be2257bbf69f20d4cedce250c7b5c
jhealey <jjheals@gmail.com>
2024-04-17 09:48:41 +0200

    added factoryTimeline

---
+++ industrialgiudecca/src/components/FactoryTimeline.js
@@ -0,0 +1,12 @@
+// src/components/FactoryTimeline.js
+
+import React, { useState, useEffect, useRef } from 'react';
+
+import '../css/components/FactoryTimeline.css';
+
+ 
+const FactoryTimeline = ({  }) => {
+
+};
+
+export default FactoryTimeline;
